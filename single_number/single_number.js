c30a806fd1faa0b5bb6370c6838d38aef39a2022
coronaisme <coronaisme@gmail.com>
2020-07-16 12:38:01 -0700

    sol

--- single_number/single_number.js
+++ single_number/single_number.js
@@ -1,3 +1,22 @@
 // Given a non-empty array of integers, 
 // every element appears twice except for one. 
 // Find that single one.
+
+// Input: [2,2,1]
+// Output: 1
+
+var singleNumber = function(nums) {
+  let obj = {}
+  nums.forEach((n) => {
+      if(n in obj) {
+          obj[n] += 1
+      } else {
+          obj[n] = 1
+      }
+  })
+  for (let key in obj) {
+      if(obj[key] === 1) {
+          return parseInt(key)
+      }
+  }
+};
