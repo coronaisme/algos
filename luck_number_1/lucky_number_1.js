8ee39f9dc617d8edc055b9317681f4dd386e22d9
coronaisme <coronaisme@gmail.com>
2020-07-16 12:35:30 -0700

    sol

--- luck_number_1/lucky_number_1.js
+++ luck_number_1/lucky_number_1.js
@@ -4,3 +4,26 @@
 // Return a lucky integer in the array. If there are multiple 
 // lucky integers return the largest of them. If there is no lucky integer return -1.
 
+var findLucky = function(arr) {
+  //iterate and make an obj where key = number and value = count of occurences
+  let obj = {}
+  let lucky = -1;
+
+  for(let i = 0; i < arr.length; i++) {
+    if( arr[i] in obj) {
+      obj[arr[i]] += 1
+    }
+    else {
+      obj[arr[i]] = 1
+    }
+  }
+    for( let key in obj) {
+      if((parseInt(key) === obj[key]) && (parseInt(key) > lucky)) {  
+        lucky = parseInt(key)
+      }
+    }
+    return lucky
+  //if key = val, return either
+  //if no keys = vals return -1
+  //if multiple key = val, return highest
+};
