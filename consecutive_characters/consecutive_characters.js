01035212299cfc1ed5a3cdf5fb8f375647c03fff
coronaisme <coronaisme@gmail.com>
2020-07-23 13:47:28 -0700

    init

---
+++ consecutive_characters/consecutive_characters.js
@@ -0,0 +1,8 @@
+// Given a string s, the power of the string is the maximum length of 
+// a non-empty substring that contains only one unique character.
+
+// Return the power of the string.
+
+// Input: s = "abbcccddddeeeeedcba"
+// Output: 5
+// Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
