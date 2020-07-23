575ca9fd9bfbfc52b3de7107272289793c1c765f
coronaisme <coronaisme@gmail.com>
2020-07-23 12:24:52 -0700

    sol

--- mid_linked_list/middle_of_linked_list.js
+++ mid_linked_list/middle_of_linked_list.js
@@ -8,3 +8,33 @@
 // The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
 // Note that we returned a ListNode object ans, such that:
 // ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+
+
+var middleNode = function(head) {
+  let count = 0
+  let currentNode = head
+  let target;
+  let finalCount = 0
+  // count the nodes
+  while (currentNode) {
+    count += 1
+    currentNode = currentNode.next
+  }
+  if (count % 2 === 0) {
+    target = (count / 2) + 1
+  } else { // odd
+    target = Math.ceil(count / 2)
+  }
+  // find middle num
+  currentNode = head
+  while (finalCount < target - 1) {
+   //  console.log("")
+   //  console.log("current node", currentNode)
+   //  console.log("target", target)
+   //  console.log("finalcount", finalCount)
+   //  console.log(" ")
+    finalCount += 1
+    currentNode = currentNode.next
+  }
+ return currentNode
+};
