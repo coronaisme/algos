6b7b946bda8835eadd974b32d664fa53a17dded4
coronaisme <coronaisme@gmail.com>
2020-07-23 13:48:20 -0700

    sol

--- consecutive_characters/consecutive_characters.js
+++ consecutive_characters/consecutive_characters.js
@@ -1,4 +1,4 @@
-// Given a string s, the power of the string is the maximum length of 
+// Given a string s, the power of the string is the maximum length of
 // a non-empty substring that contains only one unique character.
 
 // Return the power of the string.
@@ -6,3 +6,22 @@
 // Input: s = "abbcccddddeeeeedcba"
 // Output: 5
 // Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+
+const maxPower = (s) => {
+  let currentHighestCount = 0;
+  let currentInARow = 1;
+  const sArr = s.split("");
+
+  for (let i = 0; i < sArr.length; i++) {
+    if (sArr[i] !== sArr[i + 1]) {
+      currentInARow = 1;
+    }
+    if (sArr[i] === sArr[i + 1]) {
+      currentInARow += 1;
+    }
+    if (currentInARow > currentHighestCount) {
+      currentHighestCount = currentInARow;
+    }
+  }
+  return currentHighestCount;
+};
