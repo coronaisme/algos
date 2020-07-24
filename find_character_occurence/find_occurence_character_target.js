5d485632a9efbb89b2d6b7798c68320c4064a76e
coronaisme <coronaisme@gmail.com>
2020-07-24 12:59:38 -0700

    init

---
+++ find_character_occurence/find_occurence_character_target.js
@@ -0,0 +1,11 @@
+// There is a string 'str' 
+// Find the number of occurences of char 't' in 'str'
+
+// Ex :
+// str = 'akfdakfjnadtihamskfnkjsdhfkjasdfkasdnfkjhasdlfkasjnf'
+// t = 'a'
+
+
+const findOccurences = (str, t) => {
+    
+}
