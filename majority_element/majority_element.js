3f85be224b9c6556ce60b3609a4e032a66d11edd
coronaisme <coronaisme@gmail.com>
2020-07-25 08:16:49 -0700

    sol

--- majority_element/majority_element.js
+++ majority_element/majority_element.js
@@ -1,13 +1,20 @@
-// Given an array of size n, find the majority element. 
+// Given an array of size n, find the majority element.
 // The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
-// You may assume that the array is non-empty 
+// You may assume that the array is non-empty
 // and the majority element always exist in the array.
 
 // ex: input : [2,2,1,1,1,2,2]
 // output: 2
 
+const majorityElement = function (nums) {
+  const obj = {};
 
-const majorityElement = function(nums) {
+  nums.forEach((num) => (obj[num] ? obj[num]++ : (obj[num] = 1)));
 
- }
+  for (let key in obj) {
+    if (obj[key] > nums.length / 2) {
+      return parseInt(key);
+    }
+  }
+};
