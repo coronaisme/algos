fe6c437ac3d185e2abca973f2c7d5781f21fe505
coronaisme <coronaisme@gmail.com>
2020-08-07 12:59:59 -0700

    sol

--- remove_ele_from_linked_list/remove_ele_from_linked_list.js
+++ remove_ele_from_linked_list/remove_ele_from_linked_list.js
@@ -1,4 +1,4 @@
-// Remove all elements from a linked 
+// Remove all elements from a linked
 // list of integers that have value val.
 
 // Example:
@@ -7,5 +7,22 @@
 // Output: 1->2->3->4->5
 
 const removeElements = (head, val) => {
-  
-}
+  let currentNode = head;
+  if (!head) return null;
+  // we forward look; delete the future, instead of present
+  while (currentNode.next) {
+    // remove all but last
+    while (currentNode.next.val === val) {
+      // remove consecutive values
+      currentNode.next = currentNode.next.next;
+      if (currentNode.next === null) break; // the new next is last
+    }
+    if (currentNode.next) {
+      // if after removal, not at last
+      currentNode = currentNode.next; // hop to next link
+    }
+    //console.log("current node val", currentNode.val)
+  } // end of while
+  if (head.val === val) head = head.next; // handle deletion of head
+  return head;
+};
