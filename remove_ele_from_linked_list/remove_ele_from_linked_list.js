49a6a396edc5731f6e8caad941f9cf44bef29150
coronaisme <coronaisme@gmail.com>
2020-08-10 14:47:07 -0700

    init

--- remove_ele_from_linked_list/remove_ele_from_linked_list.js
+++ remove_ele_from_linked_list/remove_ele_from_linked_list.js
@@ -10,19 +10,51 @@ const removeElements = (head, val) => {
   let currentNode = head;
   if (!head) return null;
   // we forward look; delete the future, instead of present
+
   while (currentNode.next) {
     // remove all but last
+
     while (currentNode.next.val === val) {
       // remove consecutive values
+
       currentNode.next = currentNode.next.next;
       if (currentNode.next === null) break; // the new next is last
     }
+
     if (currentNode.next) {
       // if after removal, not at last
       currentNode = currentNode.next; // hop to next link
     }
+
     //console.log("current node val", currentNode.val)
   } // end of while
   if (head.val === val) head = head.next; // handle deletion of head
+
+  return head;
+};
+
+const removeElements2 = (head, val) => {
+  let prev = null;
+  let current = head;
+  //assign placeholders
+
+  while (current !== null) {
+    //once current = null we have eliminated all nodes that we dont want
+    if (current.val === val) {
+      //check for val to remove
+      if (prev === null) {
+        //check if the val we wanted to remove was head
+        head = current.next;
+      } else {
+        //if not original head, skip current to remove it
+        prev.next = current.next;
+      }
+    } else {
+      //traverse list
+      prev = current;
+    }
+    //traverse list
+    current = current.next;
+  }
   return head;
 };
