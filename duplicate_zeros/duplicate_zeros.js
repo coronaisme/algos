cf6948e8b412fda3527a91f192f78c5a2d6a108b
coronaisme <coronaisme@gmail.com>
2020-08-10 14:48:16 -0700

    sol

--- duplicate_zeros/duplicate_zeros.js
+++ duplicate_zeros/duplicate_zeros.js
@@ -1,12 +1,10 @@
-// Given a fixed length array arr of integers, 
+// Given a fixed length array arr of integers,
 // duplicate each occurrence of zero, shifting the remaining elements to the right.
 
 // Note that elements beyond the length of the original array are not written.
 
-// Do the above modifications to the input array in place, 
+// Do the above modifications to the input array in place,
 // do not return anything from your function.
-
- 
 
 // Example 1:
 
@@ -15,12 +13,12 @@
 // Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
 
 const duplicateZeros = (arr) => {
-  for(let i = arr.length - 2; i >= 0; i--) {
-    if(arr[i] === 0) {
-      for(let j = arr.length - 1; j > i; j--) {
-        arr[j] = arr[j - 1]
+  for (let i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] === 0) {
+      for (let j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
       }
     }
   }
-return null
-}
+  return null;
+};
