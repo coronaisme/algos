c27badc7b9cd1d7561a27b24ee46dd1d495a7e06
coronaisme <coronaisme@gmail.com>
2020-08-12 13:52:09 -0700

    sol

--- create_target_array_in_order/create_target_arr_in_given_order.js
+++ create_target_array_in_order/create_target_arr_in_given_order.js
@@ -1,15 +1,13 @@
-// Given two arrays of integers nums and index. 
+// Given two arrays of integers nums and index.
 // Your task is to create target array under the following rules:
 
 // Initially target array is empty.
-// From left to right read nums[i] and index[i], 
+// From left to right read nums[i] and index[i],
 // insert at index index[i] the value nums[i] in target array.
 // Repeat the previous step until there are no elements to read in nums and index.
 // Return the target array.
 
 // It is guaranteed that the insertion operations will be valid.
-
- 
 
 // Example 1:
 
@@ -24,5 +22,13 @@
 // 4            1        [0,4,1,3,2]
 
 const createArr = (nums, i) => {
-  
-}
+  // create new target array
+  const newArr = [];
+  // iterate for length of nums
+  for (let i = 0; i < nums.length; i++) {
+    newArr.splice(index[i], 0, nums[i]);
+  }
+  // for each index, insert corresponding num into newArr
+  // return new arr
+  return newArr;
+};
