2563670a79f6f7bed0867a157819be35f7006297
coronaisme <coronaisme@gmail.com>
2020-08-24 23:24:54 -0700

    init

---
+++ create_negative_numbers_in_sorted_matrix/create_negative_numbers_in_sorted_matrix.js
@@ -0,0 +1,16 @@
+// Given a m * n matrix grid which is sorted 
+// in non-increasing order both row-wise and column-wise. 
+
+// Return the number of negative numbers in grid.
+
+ 
+
+// Example 1:
+
+// Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
+
+const countNumbers = (matrix) => {
+  
+}
