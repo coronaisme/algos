34a974f3016c4fad985bb9251eea87823f96e35c
coronaisme <coronaisme@gmail.com>
2020-08-24 23:25:11 -0700

    sol1

--- create_negative_numbers_in_sorted_matrix/create_negative_numbers_in_sorted_matrix.js
+++ create_negative_numbers_in_sorted_matrix/create_negative_numbers_in_sorted_matrix.js
@@ -1,9 +1,7 @@
-// Given a m * n matrix grid which is sorted 
-// in non-increasing order both row-wise and column-wise. 
+// Given a m * n matrix grid which is sorted
+// in non-increasing order both row-wise and column-wise.
 
 // Return the number of negative numbers in grid.
-
- 
 
 // Example 1:
 
@@ -12,5 +10,17 @@
 // Explanation: There are 8 negatives number in the matrix.
 
 const countNumbers = (matrix) => {
-  
-}
+  // have a variable to hold total count of negative nums
+  let count = 0;
+  // iterate through each row
+  grid.forEach((row) => {
+    // check each number to see if its positive from the end of a row; if you get a positive / 0 go to next row; otherwise increment because its a negative
+    for (let i = row.length - 1; i >= 0; i--) {
+      if (row[i] >= 0) break;
+      count += 1;
+    }
+  });
+  // iterate through each col value in row,
+  // if its negative, add one to count
+  return count;
+};
