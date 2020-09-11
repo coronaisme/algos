b4dd716c45a71570dd642e02ff9afd3595bb161c
coronaisme <coronaisme@gmail.com>
2020-09-11 12:08:41 -0700

    init

---
+++ route_cipher/route_cipher.js
@@ -0,0 +1,23 @@
+//I am not sure if this is a real route cipher
+//Inputs are a message string, a row number r , and a column number c
+//create a grid with the characters including white spaces in r number rows and c number columns
+//the message should left to right via the rows when normal
+//the coded message would be up and down left to right via the columns instead
+
+//ex : message = "Hello, I am a message to scramblelol", r = 6, c = 6
+
+//    'H', 'e', 'l', 'l', 'o', ',' 
+//    ' ', 'I', ' ', 'a', 'm', ' ' 
+//    'a', ' ', 'm', 'e', 's', 's' 
+//    'a', 'g', 'e', ' ', 't', 'o' 
+//    ' ', 's', 'c', 'r', 'a', 'm' 
+//    'b', 'l', 'e', 'l', 'o', 'l' 
+
+//output = 'H aa beI gsll mecelae rlomstao, soml'
+
+
+const routeCipher = (message, c, r) => {
+
+
+
+}
