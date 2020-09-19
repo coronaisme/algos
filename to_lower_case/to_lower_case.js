d886e3d4e337eab344b37ef21c79c4f3cb23581c
coronaisme <coronaisme@gmail.com>
2020-09-18 20:08:14 -0700

    sol

--- to_lower_case/to_lower_case.js
+++ to_lower_case/to_lower_case.js
@@ -1,13 +1,26 @@
 // Implement function ToLowerCase() that has a string parameter str,
 //  and returns the same string in lowercase.
 
- 
-
 // Example 1:
 
 // Input: "Hello"
 // Output: "hello"
 
-const toLowerCase = (string) => {
-  
-}
+const isUpperCase = (str) => {
+  return str === str.toUpperCase();
+};
+
+const toLowerCase = function (str) {
+  let split = str.split("");
+
+  let newArr = split.map((c) => {
+    if (c.charCodeAt(0) >= 65 && c.charCodeAt(0) <= 90 && isUpperCase(c)) {
+      console.log(c, "log");
+      let code = c.charCodeAt(0) + 32;
+      return String.fromCharCode(code);
+    }
+
+    return c;
+  });
+  return newArr.join("");
+};
