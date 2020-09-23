4be0a779037eb6a25470038284d47940e9e0801b
coronaisme <coronaisme@gmail.com>
2020-09-23 15:30:34 -0700

    fixed sol

--- pow(x,n)/pow(x,n).js
+++ pow(x,n)/pow(x,n).js
@@ -5,7 +5,7 @@ const myPow = function (x, n) {
   }
   return pow(x, n);
 
-  const pow = (x, n) => {
+  function pow(x, n) {
     if (n == 0) return 1;
     const half = pow(x, Math.floor(n / 2));
     if (n % 2 === 0) {
