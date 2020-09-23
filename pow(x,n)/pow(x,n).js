fe51316259953cf6ba32d49c160521b0466bc906
coronaisme <coronaisme@gmail.com>
2020-09-23 15:29:27 -0700

    sol

--- pow(x,n)/pow(x,n).js
+++ pow(x,n)/pow(x,n).js
@@ -5,6 +5,13 @@ const myPow = function (x, n) {
   }
   return pow(x, n);
 
-  function pow(x, n) {
-   
+  const pow = (x, n) => {
+    if (n == 0) return 1;
+    const half = pow(x, Math.floor(n / 2));
+    if (n % 2 === 0) {
+      return half * half;
+    } else {
+      return half * half * x;
+    }
+  }
 };
