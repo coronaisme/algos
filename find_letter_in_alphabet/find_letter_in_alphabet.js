7362b2a7509797d5e530e940c217ec4ddcba022d
coronaisme <coronaisme@gmail.com>
2020-09-26 14:07:33 -0700

    iterative finder

---
+++ find_letter_in_alphabet/find_letter_in_alphabet.js
@@ -0,0 +1,30 @@
+//This is a problem I am creating to play with time complexity and recusion
+//The problem will involve an alphabet and a target letter
+//The goal of the algo will be to find the target letter
+//and output the amount of steps it took to get there
+
+// 97-122
+
+const alphabetMaker = () => {
+  let output = [];
+
+  for (let i = 97; i <= 122; i++) {
+    output.push(String.fromCharCode(i));
+  }
+  return output;
+};
+
+const iterativeFinder = (alphabet, target) => {
+  for (let i = 0; i <= alphabet.length; i++) {
+    if (alphabet[i] === target) {
+      return `The letter ${target} has been found, tried ${i} times`;
+    }
+  }
+
+  return `The letter ${target} has NOT been found, tried ${
+    alphabet.length + 1
+  } times`;
+};
+
+// let alphabet = alphabetMaker();
+// console.log(recursiveFinder(alphabet, "j"));
