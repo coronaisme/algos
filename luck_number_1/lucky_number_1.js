1792f26b5465786ba2c89ce846329a91ad6c7bd6
coronaisme <coronaisme@gmail.com>
2020-09-26 16:06:07 -0700

    ?

--- luck_number_1/lucky_number_1.js
+++ luck_number_1/lucky_number_1.js
@@ -1,28 +1,27 @@
-// Given an array of integers arr, a lucky integer is an integer 
+// Given an array of integers arr, a lucky integer is an integer
 // which has a frequency in the array equal to its value.
 
-// Return a lucky integer in the array. If there are multiple 
+// Return a lucky integer in the array. If there are multiple
 // lucky integers return the largest of them. If there is no lucky integer return -1.
 
-var findLucky = function(arr) {
+var findLucky = function (arr) {
   //iterate and make an obj where key = number and value = count of occurences
-  let obj = {}
+  let obj = {};
   let lucky = -1;
 
-  for(let i = 0; i < arr.length; i++) {
-    if( arr[i] in obj) {
-      obj[arr[i]] += 1
-    }
-    else {
-      obj[arr[i]] = 1
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] in obj) {
+      obj[arr[i]] += 1;
+    } else {
+      obj[arr[i]] = 1;
     }
   }
-    for( let key in obj) {
-      if((parseInt(key) === obj[key]) && (parseInt(key) > lucky)) {  
-        lucky = parseInt(key)
-      }
+  for (let key in obj) {
+    if (parseInt(key) === obj[key] && parseInt(key) > lucky) {
+      lucky = parseInt(key);
     }
-    return lucky
+  }
+  return lucky;
   //if key = val, return either
   //if no keys = vals return -1
   //if multiple key = val, return highest
