fbf2688071b75fe0c3c51ebf4767e5390db74973
coronaisme <coronaisme@gmail.com>
2020-10-22 15:50:45 -0700

    helper

--- flipping_image/flipping_image.js
+++ flipping_image/flipping_image.js
@@ -1,11 +1,11 @@
-// Given a binary matrix A, we want to flip the image horizontally, 
+// Given a binary matrix A, we want to flip the image horizontally,
 // then invert it, and return the resulting image.
 
-// To flip an image horizontally means that each row of the image is reversed. 
+// To flip an image horizontally means that each row of the image is reversed.
 //  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
 
-// To invert an image means that each 0 is replaced by 1, 
-// and each 1 is replaced by 0. For example, inverting [0, 1, 1] 
+// To invert an image means that each 0 is replaced by 1,
+// and each 1 is replaced by 0. For example, inverting [0, 1, 1]
 // results in [1, 0, 0].
 
 // Example 1:
@@ -15,6 +15,14 @@
 // Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
 // Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
 
-const flipAndInvertImage = (input) => {
+const invert = (arr) => {
+  return arr.map((el) => {
+    if (el === 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
 
-}
+const flipAndInvertImage = (input) => {};
