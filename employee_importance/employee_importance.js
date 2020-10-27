ba01688af46d4383d017f4a15ff66f1f3f00c0ff
coronaisme <coronaisme@gmail.com>
2020-10-26 18:41:19 -0700

    recursive sol

--- employee_importance/employee_importance.js
+++ employee_importance/employee_importance.js
@@ -1,16 +1,16 @@
-// You are given a data structure of employee information, 
-// which includes the employee's unique id, their importance value 
+// You are given a data structure of employee information,
+// which includes the employee's unique id, their importance value
 // and their direct subordinates' id.
 
-// For example, employee 1 is the leader of employee 2, 
-// and employee 2 is the leader of employee 3. They have importance value 15, 
-// 10 and 5, respectively. Then employee 1 has a data structure like [1, 15, [2]], 
-// and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. 
-// Note that although employee 3 is also a subordinate of employee 1, 
+// For example, employee 1 is the leader of employee 2,
+// and employee 2 is the leader of employee 3. They have importance value 15,
+// 10 and 5, respectively. Then employee 1 has a data structure like [1, 15, [2]],
+// and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []].
+// Note that although employee 3 is also a subordinate of employee 1,
 // the relationship is not direct.
 
-// Now given the employee information of a company, 
-// and an employee id, you need to return the total importance value of this employee and 
+// Now given the employee information of a company,
+// and an employee id, you need to return the total importance value of this employee and
 // all their subordinates.
 
 // Example 1:
@@ -18,13 +18,31 @@
 // Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
 // Output: 11
 // Explanation:
-// Employee 1 has importance value 5, and he has two direct subordinates: 
-// employee 2 and employee 3. They both have importance value 3. 
+// Employee 1 has importance value 5, and he has two direct subordinates:
+// employee 2 and employee 3. They both have importance value 3.
 // So the total importance value of employee 1 is 5 + 3 + 3 = 11.
 
-var GetImportance = function(employees, id) {
-
+var GetImportance = function (employees, id) {
   let importance = 0;
-  let employee = employees.find(employee => employee.id === id)
-    
+  let employee = employees.find((employee) => employee.id === id);
+
+  function importanceCount(employee) {
+    //base case
+    if (employee.subordinates.length === 0) {
+      console.log(employee, "employee");
+      importance += employee.importance;
+    } else {
+      //recursive case
+      console.log(employee);
+      employee.subordinates.forEach((subId) => {
+        let e = employees.find((employee) => employee.id === subId);
+        console.log(e, "e");
+        importanceCount(e);
+      });
+      importance += employee.importance;
+    }
+  }
+
+  importanceCount(employee);
+  return importance;
 };
