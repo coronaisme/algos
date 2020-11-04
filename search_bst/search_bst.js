81044f6baf47ff51d350806585a5aacab638cc24
coronaisme <coronaisme@gmail.com>
2020-11-04 13:55:21 -0800

    sol

--- search_bst/search_bst.js
+++ search_bst/search_bst.js
@@ -1,10 +1,10 @@
-// Given the root node of a binary search tree (BST) and a value. 
-// You need to find the node in the BST that the node's value equals 
-// the given value. Return the subtree rooted with that node. 
+// Given the root node of a binary search tree (BST) and a value.
+// You need to find the node in the BST that the node's value equals
+// the given value. Return the subtree rooted with that node.
 // If such node doesn't exist,
 // you should return NULL.
 
-// For example, 
+// For example,
 
 // Given the tree:
 //         4
@@ -16,10 +16,17 @@
 // And the value to search: 2
 // You should return this subtree:
 
-//       2     
-//      / \   
+//       2
+//      / \
 //     1   3
 
 const searchBST = (root, val) => {
-  
-}
+  if (root === null) {
+    return null;
+  } else if (val > root.val) {
+    return searchBST(root.right, val);
+  } else if (val < root.val) {
+    return searchBST(root.left, val);
+  }
+  return root;
+};
