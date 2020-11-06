dae497e74fc8173c85f9416f9d41717378a3c5d0
coronaisme <coronaisme@gmail.com>
2020-11-06 14:25:35 -0800

    sol

--- height_checker/height_checker.js
+++ height_checker/height_checker.js
@@ -1,20 +1,18 @@
-// Students are asked to stand in non-decreasing order of heights 
+// Students are asked to stand in non-decreasing order of heights
 // for an annual photo.
 
-// Return the minimum number of students that must move in order 
+// Return the minimum number of students that must move in order
 // for all students to be standing in non-decreasing order of height.
 
-// Notice that when a group of students is selected they can reorder 
-// in any possible way between themselves and the non selected students 
+// Notice that when a group of students is selected they can reorder
+// in any possible way between themselves and the non selected students
 // remain on their seats.
-
- 
 
 // Example 1:
 
 // Input: heights = [1,1,4,2,1,3]
 // Output: 3
-// Explanation: 
+// Explanation:
 // Current array : [1,1,4,2,1,3]
 // Target array  : [1,1,1,2,3,4]
 // On index 2 (0-based) we have 4 vs 1 so we have to move this student.
@@ -22,5 +20,14 @@
 // On index 5 (0-based) we have 3 vs 4 so we have to move this student.
 
 const heightChecker = (heights) => {
-  
-}
+  const targetArr = [...heights].sort((a, b) => a - b);
+  let result = 0;
+
+  heights.forEach((e, i) => {
+    if (heights[i] !== targetArr[i]) {
+      result++;
+    }
+  });
+
+  return result;
+};
