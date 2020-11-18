dbd11d50eb04f69c27549117d2652f5bacd298e3
coronaisme <coronaisme@gmail.com>
2020-11-18 13:14:40 -0800

    real sol, last commit was init

--- sort_array_by_parity_2/sort_array_by_parity_2.js
+++ sort_array_by_parity_2/sort_array_by_parity_2.js
@@ -1,12 +1,10 @@
 // Given an array A of non-negative integers, half of the integers in A are odd,
 //  and half of the integers are even.
 
-// Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, 
+// Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even,
 // i is even.
 
 // You may return any answer array that satisfies this condition.
-
- 
 
 // Example 1:
 
@@ -15,5 +13,30 @@
 // Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
 
 const sortArrayByParityII = (A) => {
-  
-}
+  //probably a way better way to do this - took me ~5 min
+  let odds = [];
+  let evens = [];
+  let res = [];
+
+  A.forEach((c) => {
+    //create odd and evens arrays
+    if (c % 2 === 0) {
+      evens.push(c);
+    } else {
+      odds.push(c);
+    }
+  });
+
+  A.forEach((c, i) => {
+    //place into res array in order
+    if (i % 2 === 0) {
+      res.push(evens[0]);
+      evens.splice(0, 1);
+    } else {
+      res.push(odds[0]);
+      odds.splice(0, 1);
+    }
+  });
+
+  return res;
+};
