c0ddedbb7f083b73e1dd178be2c43449e6c7edd7
coronaisme <coronaisme@gmail.com>
2020-11-19 15:01:37 -0800

    sol

--- fizz_buzz/fizz_buzz.js
+++ fizz_buzz/fizz_buzz.js
@@ -27,4 +27,20 @@
 //   "FizzBuzz",
 // ];
 
-const fizzBuzz = (n) => {};
+const fizzBuzz = (n) => {
+  let res = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      res.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      res.push("Fizz");
+    } else if (i % 5 === 0) {
+      res.push("Buzz");
+    } else {
+      res.push(i.toString());
+    }
+  }
+
+  return res;
+};
