3b5478c8ea1069a3becec3bd662c6971c32a86c5
coronaisme <coronaisme@gmail.com>
2020-11-20 13:04:22 -0800

    init

---
+++ distribute_candles/distribute_candles.js
@@ -0,0 +1,25 @@
+// Alice has n candies, where the ith candy is of type candyType[i]. 
+// Alice noticed that she started to gain weight, 
+// that is why she visited a doctor.
+
+// The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). 
+// Alice likes her candies very much. She wants to eat the maximum number of different 
+// types of candies.
+
+// Given the integer array candyType of length n, return the maximum number of different 
+// types of candies she can eat, achieving the doctor's advice.
+
+ 
+
+// Example 1:
+
+// Input: candyType = [1,1,2,2,3,3]
+// Output: 3
+// Explanation: Alice should eat only 3 candies, she currently has 2 candies of the first type, 
+// 2 candies of the second type, and 2 candies of the third type.
+// Alice will choose to eat 1 candy of each type. This leads her to eat 3 different types of 
+// candies and we return 3.
+
+const distributeCandies = (candyType) => {
+    
+};
