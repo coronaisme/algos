a9702b7900585a6f5cdf5c76841d06e2f93bd85b
coronaisme <coronaisme@gmail.com>
2020-11-25 12:36:03 -0800

    partial

--- intersection_2_arrs/intersection_2_arrs.js
+++ intersection_2_arrs/intersection_2_arrs.js
@@ -9,7 +9,17 @@
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 // Output: [9,4]
 
-
 const intersection = (nums1, nums2) => {
+  //compare the arrays, if we find numbers that are in both, push into new array
+  let newArr = [];
 
-}
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) {
+        newArr.push(nums2[j]);
+      }
+    }
+  }
+  return uniq(newArr);
+  //make uniq function and call on new array or push only if not in array
+};
