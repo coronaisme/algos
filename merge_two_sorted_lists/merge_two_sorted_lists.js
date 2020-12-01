ddb31b7dc740f3b524d0b3826bb07b17f5e8cfc2
coronaisme <coronaisme@gmail.com>
2020-12-01 13:21:07 -0800

    edge

--- merge_two_sorted_lists/merge_two_sorted_lists.js
+++ merge_two_sorted_lists/merge_two_sorted_lists.js
@@ -1,10 +1,7 @@
-// Merge two sorted linked lists and return it as a new sorted list. 
+// Merge two sorted linked lists and return it as a new sorted list.
 // The new list should be made by splicing together the nodes of the first two lists.
 
- 
-
 // Example 1:
-
 
 // Input: l1 = [1,2,4], l2 = [1,3,4]
 // Output: [1,1,2,3,4,4]
@@ -14,5 +11,7 @@
 // Output: []
 
 const mergeTwoLists = (l1, l2) => {
-  
-}
+  if (!l1 || !l2) {
+    return l1 || l2;
+  }
+};
