6da926819bedd2a60493f022c2d5adac87662c9a
coronaisme <coronaisme@gmail.com>
2020-12-01 13:22:10 -0800

    sol

--- merge_two_sorted_lists/merge_two_sorted_lists.js
+++ merge_two_sorted_lists/merge_two_sorted_lists.js
@@ -14,4 +14,15 @@ const mergeTwoLists = (l1, l2) => {
   if (!l1 || !l2) {
     return l1 || l2;
   }
+
+  let node = null;
+
+  if (l1.val < l2.val) {
+    node = l1;
+    node.next = mergeTwoLists(l1.next, l2);
+  } else {
+    node = l2;
+    node.next = mergeTwoLists(l1, l2.next);
+  }
+  return node;
 };
