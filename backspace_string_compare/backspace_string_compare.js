1abf79ed169c16362f295f73771d34b3d2542472
coronaisme <coronaisme@gmail.com>
2020-12-04 14:05:18 -0800

    sol1

--- backspace_string_compare/backspace_string_compare.js
+++ backspace_string_compare/backspace_string_compare.js
@@ -1,4 +1,4 @@
-// Given two strings S and T, return if they are equal when 
+// Given two strings S and T, return if they are equal when
 // both are typed into empty text editors. # means a backspace character.
 
 // Note that after backspacing an empty text, the text will continue empty.
@@ -15,5 +15,24 @@
 // Explanation: Both S and T become "".
 
 const backspaceCompare = (S, T) => {
-  
-}
+  let stackS = [];
+  let stackT = [];
+
+  for (let c of S) {
+    if (c == "#") {
+      stackS.pop();
+    } else {
+      stackS.push(c);
+    }
+  }
+
+  for (let c of T) {
+    if (c == "#") {
+      stackT.pop();
+    } else {
+      stackT.push(c);
+    }
+  }
+
+  return stackS.join(",") === stackT.join(",") ? true : false;
+};
