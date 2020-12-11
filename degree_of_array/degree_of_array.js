2ee97c901db13daf03b20d88009fea3318d6d821
coronaisme <coronaisme@gmail.com>
2020-12-11 13:46:19 -0800

    sol

--- degree_of_array/degree_of_array.js
+++ degree_of_array/degree_of_array.js
@@ -1,21 +1,42 @@
-// Given a non-empty array of non-negative integers nums, 
+// Given a non-empty array of non-negative integers nums,
 // the degree of this array is defined as the maximum frequency of any one of its elements.
 
-// Your task is to find the smallest possible length of a (contiguous) 
+// Your task is to find the smallest possible length of a (contiguous)
 // subarray of nums, that has the same degree as nums.
-
- 
 
 // Example 1:
 
 // Input: nums = [1,2,2,3,1]
 // Output: 2
-// Explanation: 
+// Explanation:
 // The input array has a degree of 2 because both elements 1 and 2 appear twice.
 // Of the subarrays that have the same degree:
 // [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
 // The shortest length is 2. So return 2.
 
 const findShortestSubArray = (nums) => {
-    
+  let obj = {},
+    max = 0,
+    result = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    if (!obj[nums[i]]) {
+      obj[nums[i]] = [i];
+    } else {
+      let t = obj[nums[i]];
+      t.push(i);
+      obj[nums[i]] = t;
+    }
+  }
+
+  for (let [key, value] of Object.entries(obj)) {
+    if (value.length === max) {
+      result = Math.min(result, value[value.length - 1] - value[0] + 1);
+    }
+    if (value.length > max) {
+      result = value[value.length - 1] - value[0] + 1;
+      max = value.length;
+    }
+  }
+
+  return result;
 };
