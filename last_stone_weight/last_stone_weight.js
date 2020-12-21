1b38c1a2252f3c7700bd82f1982cee86eba8d240
coronaisme <coronaisme@gmail.com>
2020-12-21 13:52:24 -0800

    working sol

--- last_stone_weight/last_stone_weight.js
+++ last_stone_weight/last_stone_weight.js
@@ -20,9 +20,9 @@
 // we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
 
 const lastStoneWeight = (stones) => {
-  if( stones.length === 1 ) return stones[0]
-  else
-  while(stones.length > 1) {
-    stones.sort((a, b) => a - b).concat(stones.pop() - stones.pop())
+  while (stones.length > 1) {
+    stones.sort((a, b) => a - b);
+    stones.push(stones.pop() - stones.pop());
   }
+  if (stones.length === 1) return stones[0];
 };
