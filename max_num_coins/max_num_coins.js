efa75acccaaa019c2098aa2005a5bdca52e3e0da
coronaisme <coronaisme@gmail.com>
2020-12-31 12:43:50 -0800

    sol

--- max_num_coins/max_num_coins.js
+++ max_num_coins/max_num_coins.js
@@ -1,4 +1,4 @@
-// There are 3n piles of coins of varying size, 
+// There are 3n piles of coins of varying size,
 // you and your friends will take piles of coins as follows:
 
 // In each step, you will choose any 3 piles of coins (not necessarily consecutive).
@@ -10,18 +10,24 @@
 
 // Return the maximum number of coins which you can have.
 
- 
-
 // Example 1:
 
 // Input: piles = [2,4,1,2,7,8]
 // Output: 9
-// Explanation: Choose the triplet (2, 7, 8), Alice Pick the pile with 8 coins, 
+// Explanation: Choose the triplet (2, 7, 8), Alice Pick the pile with 8 coins,
 // you the pile with 7 coins and Bob the last one.
-// Choose the triplet (1, 2, 4), Alice Pick the pile with 4 coins, 
+// Choose the triplet (1, 2, 4), Alice Pick the pile with 4 coins,
 // you the pile with 2 coins and Bob the last one.
 // The maximum number of coins which you can have are: 7 + 2 = 9.
 // On the other hand if we choose this arrangement (1, 2, 8), (2, 4, 7)
 //  you only get 2 + 4 = 6 coins which is not optimal.
 
-const maxCoins = (piles) => {}
+const maxCoins = (piles) => {
+  let sortedPiles = piles.sort((a, b) => a - b);
+  let coins = 0;
+
+  for (let i = sortedPiles.length / 3; i < sortedPiles.length; i += 2) {
+    coins += piles[i];
+  }
+  return coins;
+};
