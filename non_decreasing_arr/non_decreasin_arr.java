ddb99945ff48f44e819879b0f49ab777385912c2
coronaisme <coronaisme@gmail.com>
2021-01-06 11:46:54 -0800

    sol

--- non_decreasing_arr/non_decreasin_arr.java
+++ non_decreasing_arr/non_decreasin_arr.java
@@ -15,9 +15,9 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
         int count = 0;
-        for(int i = 1; i<nums.length; i++) {
-            if(nums[i-1]>nums[i]) {
-                if(i != nums.length-1 && i != 1 && nums[i-1] > nums[i+1] && nums[i-2] > nums[i]) return false;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i-1] > nums[i]) {
+                if(i != nums.length - 1 && i != 1 && nums[i-1] > nums[i+1] && nums[i-2] > nums[i]) return false;
                 count++;
                 if(count > 1) return false;
             }
